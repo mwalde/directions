3a8a22faa7dc61c31202e2ade2d6121fce225f7d
Matt <matt@macky.attlocal.net>
2016-10-03 21:49:13 -0400

    initial

---
+++ directions/mydirect.py
@@ -0,0 +1,17 @@
+import simplejson, urllib
+
+orig_coord = ('46256', 'Greenwood')
+dest_coord = ('indianpolis', '46241')
+
+
+for orig in orig_coord:
+	for dest in dest_coord:
+		url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(orig_coord),str(dest_coord))
+		result= simplejson.load(urllib.urlopen(url))
+		print(result)
+		#driving_time = result['rows'][0]['elements'][0]['duration']['value']
+		print(orig)
+		print(dest)
+		#print( driving_time/60.0/60.0)
+
+
